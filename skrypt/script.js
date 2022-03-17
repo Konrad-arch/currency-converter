a9320c6b702d4bc05372c74c101eb402edbf3d62
Konrad Kapuściński <konradkapuscinski95@gmail.com>
2022-03-17 13:03:56 +0100

    initial commit

---
+++ skrypt/script.js
@@ -0,0 +1,30 @@
+const RatEUR = 4.57;
+const RatUSD = 4.08;
+
+const formElement = document.querySelector(".js-form");
+const amountElement = document.querySelector(".js-amount");
+const currencyElement = document.querySelector(".js-currency");
+const resultElement = document.querySelector(".js-result");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const currency = currencyElement.value;
+  const amount = parseFloat(amountElement.value);
+
+  let result;
+
+  switch (currency) {
+    case "EUR":
+      result = amount / RatEUR;
+      break;
+
+    case "USD":
+      result = amount / RatUSD;
+      break;
+  }
+
+  resultElement.innerHTML = `Wynik: ${amount.toFixed(
+    2
+  )} PLN = <strong> ${result.toFixed(2)} ${currency} </strong>`;
+});
