3d6f0b7b7676af145034cc2b69e445d0a48e65d2
Konrad Kapuściński <konradkapuscinski95@gmail.com>
2022-03-21 13:38:29 +0100

    add bloks

--- skrypt/script.js
+++ skrypt/script.js
@@ -1,31 +1,34 @@
-const RatEUR = 4.57;
-const RatUSD = 4.08;
+{
+  const RatEUR = 4.57;
+  const RatUSD = 4.08;
 
-const formElement = document.querySelector(".js-form");
-const amountElement = document.querySelector(".js-amount");
-const currencyElement = document.querySelector(".js-currency");
-const resultElement = document.querySelector(".js-result");
+  const formElement = document.querySelector(".js-form");
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+  const resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  const currency = currencyElement.value;
-  const amount = parseFloat(amountElement.value);
+    const currency = currencyElement.value;
+    const amount = parseFloat(amountElement.value);
 
-  let result;
+    let result;
 
-  switch (currency) {
-    case "EUR":
-      result = amount / RatEUR;
-      break;
+    switch (currency) {
+      case "EUR":
+        result = amount / RatEUR;
+        break;
 
-    case "USD":
-      result = amount / RatUSD;
-      break;
-  }
-  {
-    resultElement.innerHTML = `Wynik: ${amount.toFixed(
-      2
-    )} PLN = <strong> ${result.toFixed(2)} ${currency} </strong>`;
-  }
-});
+      case "USD":
+        result = amount / RatUSD;
+        break;
+    }
+
+    {
+      resultElement.innerHTML = `Wynik: ${amount.toFixed(
+        2
+      )} PLN = <strong> ${result.toFixed(2)} ${currency} </strong>`;
+    }
+  });
+}
